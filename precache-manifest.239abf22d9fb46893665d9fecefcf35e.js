4ae14454eeaa3ddfe97ebb2b73fd52d71d8bcfb0
Andrey Kashapov <kashapovandrey@gmail.com>
2019-10-24 17:09:07 +0300

    add gh-pages

---
+++ precache-manifest.239abf22d9fb46893665d9fecefcf35e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f801519711e536df8da8edbc5ab2508",
+    "url": "/signatures/index.html"
+  },
+  {
+    "revision": "f762814bcdca6dc0b30a",
+    "url": "/signatures/static/css/main.0417e8d8.chunk.css"
+  },
+  {
+    "revision": "c277cdd3744579d0a437",
+    "url": "/signatures/static/js/2.dc7ca779.chunk.js"
+  },
+  {
+    "revision": "f762814bcdca6dc0b30a",
+    "url": "/signatures/static/js/main.509e6c75.chunk.js"
+  },
+  {
+    "revision": "3cd48047bd858cad5564",
+    "url": "/signatures/static/js/runtime-main.a26e49a3.js"
+  },
+  {
+    "revision": "acf8252599df9c602a39a8602441fd60",
+    "url": "/signatures/static/media/spinner.acf82525.svg"
+  }
+]);
