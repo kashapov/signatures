d5049a2a5d72a36065acc2c85075b40dccbedd0e
Andrey Kashapov <kashapovandrey@gmail.com>
2019-10-24 17:19:04 +0300

    update links

---
+++ build/precache-manifest.754ce9ceb9c489658c5ee7e7cb16a13b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa28ccc2f71a3ca7fb99928838999a7",
+    "url": "/signatures/index.html"
+  },
+  {
+    "revision": "ba50aaafeb78d676e9a8",
+    "url": "/signatures/static/css/main.0417e8d8.chunk.css"
+  },
+  {
+    "revision": "c277cdd3744579d0a437",
+    "url": "/signatures/static/js/2.dc7ca779.chunk.js"
+  },
+  {
+    "revision": "ba50aaafeb78d676e9a8",
+    "url": "/signatures/static/js/main.274dc97c.chunk.js"
+  },
+  {
+    "revision": "3cd48047bd858cad5564",
+    "url": "/signatures/static/js/runtime-main.a26e49a3.js"
+  },
+  {
+    "revision": "acf8252599df9c602a39a8602441fd60",
+    "url": "/signatures/static/media/spinner.acf82525.svg"
+  }
+]);
